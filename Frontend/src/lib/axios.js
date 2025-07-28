8899b890cd8b669250a94ab750d9f889371ba105
mahzaz1 <malikahzaz301@gmail.com>
2025-07-28 22:06:15 +0500

    fixes

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: true,
 });
-
--- Frontend/src/store/authStore.js
+++ Frontend/src/store/authStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/";
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
